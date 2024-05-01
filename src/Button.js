816f7fb2ebe70480a1457698f84f0056b5399f97
Catalina Simonovich <catalinasimonovich1@gmail.com>
2024-05-01 15:25:28 -0300

    Modify toggle button and add style to it

--- src/Button.js
+++ src/Button.js
@@ -1,10 +1,18 @@
 
 function Button({ onClick }) {
     return ( 
-            <label className="switch slider">
+           /*<label className="switch slider">
                 <input type="checkbox" onClick={onClick}/>
                 <span className="slider"></span>
-            </label>
+            </label>*/
+            <div className="div-button">
+                <div className="checkbox-wrapper-22">
+                    <label className="switch" type="checkbox">
+                        <input type="checkbox" id="checkbox" onClick={onClick}/>
+                        <div className="slider round"></div>
+                    </label>
+                </div>
+            </div>
          );
 }
 
